7e4117a6d8782be0c2c6139a1f768fe808dcb4ef
Shayan <shayankhoshbakht.2000@gmail.com>
2026-02-24 13:24:10 +0330

    crud for user

--- src/controllers/usercontroller.js
+++ src/controllers/usercontroller.js
@@ -12,3 +12,84 @@ exports.createUser = async (req, res) => {
     });
   }
 };
+exports.getOne = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const user = await User.findById(id);
+    if (!user) {
+      return res.status(404).json({
+        message: "user not found",
+      });
+    }
+    res.status(200).json({
+      message: "user fatched successfully",
+      user,
+    });
+  } catch (error) {
+    res.status(400).json({
+      message: error.message,
+    });
+  }
+};
+exports.getAll = async (req, res) => {
+  try {
+    const users = await User.find();
+    res.status(200).json({
+      status: "success",
+      result: users.length,
+      users,
+    });
+  } catch (error) {
+    res.status(400).json({
+      message: error.message,
+    });
+  }
+};
+exports.deleteUser = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const user = await User.findByIdAndDelete(id);
+    if (!id) {
+      res.status(404).json({
+        message: "user not found",
+      });
+    }
+    res.status(200).json({
+      status: "success",
+      message: "user deleted successfully",
+      user,
+    });
+  } catch (error) {
+    res.status(400).json({
+      message: error.message,
+    });
+  }
+};
+exports.updateUser = async (req, res) => {
+  try {
+    const allowedUpdates = ["name", "email"];
+    const updates = {};
+    allowedUpdates.forEach((key) => {
+      if (req.body[key] !== undefined) updates[key] = req.body[key];
+    });
+    const { id } = req.params;
+    const user = await User.findByIdAndUpdate(id, updates, {
+      new: true,
+      runValidators: true,
+    }).select("-password");
+    if (!user) {
+      return res
+        .status(404)
+        .json({ status: "fail", message: "user not found" });
+    }
+    res.status(200).json({
+      status: "success",
+      data: { user },
+    });
+  } catch (error) {
+    res.status(400).json({
+      status: "fail",
+      message: error.message,
+    });
+  }
+};
--- src/utils/userRoutes.js
+++ src/utils/userRoutes.js
@@ -2,4 +2,8 @@ const express = require("express");
 const usercontroller = require("../controllers/usercontroller");
 const Route = express.Router();
 Route.route("/singup").post(usercontroller.createUser);
+Route.route("/:id").get(usercontroller.getOne);
+Route.route("/").get(usercontroller.getAll);
+Route.route("/:id").delete(usercontroller.deleteUser);
+Route.route("/:id").patch(usercontroller.updateUser);
 module.exports = Route;
